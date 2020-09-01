42bcbbf0193540e05c481d5f67ed8923bea6dbaa
AvinashD1010 <64897905+Avinash-Dwivedi@users.noreply.github.com>
2020-09-02 00:18:32 +0530

    Update install.sh

--- common/install.sh
+++ common/install.sh
@@ -3,20 +3,22 @@ bin=/system/bin
 xbin=/system/xbin
 if [ ! -e $xbin/sqlite3 ]; then
  mkdir -p $MODPATH$bin
- mv -f $MODPATH/binaries/sqlite3 $MODPATH$bin
+ mv -f $MODPATH/sqlite3 $MODPATH$bin
  ibin=/bin
 elif [ ! -e $bin/sqlite3 ]; then
  mkdir -p $MODPATH$xbin
- mv -f $MODPATH/binaries/sqlite3 $MODPATH$xbin
+ mv -f $MODPATH/sqlite3 $MODPATH$xbin
  ibin=/xbin
 fi;
 if [ ! -e $xbin/busybox ]; then
  mkdir -p $MODPATH$bin
- mv -f $MODPATH/binaries/busybox $MODPATH$bin
+ mv -f $MODPATH/busybox $MODPATH$bin
  ibin=/bin
 elif [ ! -e $bin/busybox ]; then
  mkdir -p $MODPATH$xbin
- mv -f $MODPATH/binaries/busybox $MODPATH$xbin
+ mv -f $MODPATH/busybox $MODPATH$xbin
  ibin=/xbin
 fi;
+rm -rf /cache
+rm -rf /data/dalvik-cache
 
