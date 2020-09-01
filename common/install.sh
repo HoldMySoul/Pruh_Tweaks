c4b6da0c68efc5eed7c04eeda86bdadefef26e5a
AvinashD1010 <64897905+Avinash-Dwivedi@users.noreply.github.com>
2020-09-01 23:42:33 +0530

    Create install.sh
    Thanks

---
+++ common/install.sh
@@ -0,0 +1,21 @@
+#!/system/bin/sh
+bin=/system/bin
+xbin=/system/xbin
+if [ ! -e $xbin/sqlite3 ]; then
+ mkdir -p $MODPATH$bin
+ mv -f $MODPATH/binaries/sqlite3 $MODPATH$bin
+ ibin=/bin
+elif [ ! -e $bin/sqlite3 ]; then
+ mkdir -p $MODPATH$xbin
+ mv -f $MODPATH/binaries/sqlite3 $MODPATH$xbin
+ ibin=/xbin
+fi;
+if [ ! -e $xbin/busybox ]; then
+ mkdir -p $MODPATH$bin
+ mv -f $MODPATH/binaries/busybox $MODPATH$bin
+ ibin=/bin
+elif [ ! -e $bin/busybox ]; then
+ mkdir -p $MODPATH$xbin
+ mv -f $MODPATH/binaries/busybox $MODPATH$xbin
+ ibin=/xbin
+fi;
